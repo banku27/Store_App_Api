47479b80d8ad0da72eeb50a4797df3a2ecb159df
banku27 <pankajparihar123321@gmail.com>
2022-09-07 00:01:03 +0530

    corousel

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:store_api_app/consts/global_colors.dart';
@@ -22,6 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
@@ -40,11 +42,11 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: TextField(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Column(
+            children: [
+              TextField(
                 controller: searchController,
                 keyboardType: TextInputType.text,
                 decoration: InputDecoration(
@@ -68,12 +70,22 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: SaleWidget(),
-            ),
-          ],
+              SizedBox(
+                height: size.height * 0.25,
+                child: Swiper(
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return const SaleWidget();
+                  },
+                  pagination: const SwiperPagination(
+                    alignment: Alignment.bottomCenter,
+                    builder: DotSwiperPaginationBuilder(
+                        color: Colors.white, activeColor: Colors.red),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
