85464ad3ed1b2bd7100b6e8707f4b6d40b07c572
banku27 <pankajparihar123321@gmail.com>
2022-09-07 21:13:54 +0530

    product model

---
+++ lib/model/product_model.dart
@@ -0,0 +1,29 @@
+import 'package:store_api_app/model/categories_model.dart';
+
+class ProductsModel {
+  int? id;
+  String? title;
+  int? price;
+  String? description;
+  CategoriesModel? category;
+  List<String>? images;
+
+  ProductsModel(
+      {this.id,
+      this.title,
+      this.price,
+      this.description,
+      this.category,
+      this.images});
+
+  ProductsModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    price = json['price'];
+    description = json['description'];
+    category = json['category'] != null
+        ? CategoriesModel.fromJson(json['category'])
+        : null;
+    images = json['images'].cast<String>();
+  }
+}
